e3596ed505de8ab3eb2db09e2c987b8146193475
Michael Whalen <michael.whalen@sitechhs.com>
2019-10-15 19:54:13 -0400

    async await

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
-const displayName = document.querySelector('.pkmn-name-size');
-const displayImageFront = document.querySelector('.display-image-front-def');
-const displayImageBack = document.querySelector('.display-image-back-def');
-const displayImageShinyFront = document.querySelector('.display-image-shiny-front');
-const displayImageShinyBack = document.querySelector('.display-image-shiny-back');
-const displayNum = document.querySelector('.pkmn-num');
-const type = document.querySelector('.type');
-const input = document.getElementById('pkmn-form');
-
-input.addEventListener('keypress', function(event) {
+const DOMStrings = {
+    displayName: document.querySelector(".pkmn-name-size"),
+    displayImageFront: document.querySelector(".display-image-front-def"),
+    displayImageBack: document.querySelector(".display-image-back-def"),
+    displayImageShinyFront: document.querySelector(".display-image-shiny-front"),
+    displayImageShinyBack: document.querySelector(".display-image-shiny-back"),
+    displayNum: document.querySelector(".pkmn-num"),
+    type: document.querySelector(".type"),
+    input: document.getElementById("pkmn-form")
+  };
+  
+   DOMStrings.input.addEventListener("keypress", function(event) {
     if (event.keyCode === 13) {
-        event.preventDefault();
-        document.querySelector(".nes-btn").click();
-      }
-    });
-
-function init() {
-    document.getElementById('pkmn-form').addEventListener('submit', function(e){
-        e.preventDefault();
-        const pkmnName = document.querySelector('#pokemon-name').value;
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnName}`)
+      event.preventDefault();
+      document.querySelector(".nes-btn").click();
+    }
+  });
+   
+  function init() {
+    document.getElementById("pkmn-form").addEventListener("submit", function(e) {
+      e.preventDefault();
+      const pkmnName = document.querySelector("#pokemon-name").value;
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnName}`)
         .then(result => {
-            return result.json();
+          return result.json();
         })
         .then(data => {
-            displayName.innerText = data.name;
-            displayNum.innerText = data.id;
-            displayImageFront.src = data.sprites.front_default;
-            displayImageBack.src = data.sprites.back_default;
-            displayImageShinyBack.src = data.sprites.back_shiny;
-            displayImageShinyFront.src = data.sprites.front_shiny;
-            //type.textContent = `Type: ${data.types[0].type.name}`;
-            type.textContent = data.types.map(data => data.type.name);
-            console.log(data);
-            
-        })
-        document.querySelector('#pokemon-name').value = '';
+          DOMStrings.displayName.innerText = data.name;
+          DOMStrings.displayNum.innerText = data.id;
+          DOMStrings.displayImageFront.src = data.sprites.front_default;
+          DOMStrings.displayImageBack.src = data.sprites.back_default;
+          DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
+          DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
+          DOMStrings.type.textContent = data.types.map(data => data.type.name);
+        });
+      document.querySelector("#pokemon-name").value = "";
     });
-    
-}
-init(); 
+  }
+  init();
+  
 
 
 /* async function test(pkmnName){
--- app2.js
+++ app2.js
@@ -9,31 +9,38 @@ const DOMStrings = {
   input: document.getElementById("pkmn-form")
 };
 
- DOMStrings.input.addEventListener("keypress", function(event) {
+DOMStrings.input.addEventListener("keypress", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     document.querySelector(".nes-btn").click();
   }
 });
- 
+
+
 function init() {
-  document.getElementById("pkmn-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const pkmnName = document.querySelector("#pokemon-name").value;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnName}`)
-      .then(result => {
-        return result.json();
-      })
-      .then(data => {
-        DOMStrings.displayName.innerText = data.name;
-        DOMStrings.displayNum.innerText = data.id;
-        DOMStrings.displayImageFront.src = data.sprites.front_default;
-        DOMStrings.displayImageBack.src = data.sprites.back_default;
-        DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
-        DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
-        DOMStrings.type.textContent = data.types.map(data => data.type.name);
-      });
-    document.querySelector("#pokemon-name").value = "";
-  });
+  document.getElementById("pkmn-form").addEventListener("submit", async function(e) {
+      e.preventDefault();
+      const pkmnName = document.querySelector("#pokemon-name").value;
+      try{
+        const result = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pkmnName}`
+        );
+        const data = await result.json();
+        const displayPkm = function(data) {
+          DOMStrings.displayName.innerText = data.name;
+          DOMStrings.displayNum.innerText = data.id;
+          DOMStrings.displayImageFront.src = data.sprites.front_default;
+          DOMStrings.displayImageBack.src = data.sprites.back_default;
+          DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
+          DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
+          DOMStrings.type.textContent = data.types.map(data => data.type.name);
+        };
+        displayPkm(data);
+        document.querySelector("#pokemon-name").value = "";
+      }catch(err){
+        console.log(err);
+      }
+
+    });
 }
 init();
