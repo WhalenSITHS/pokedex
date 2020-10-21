876dedcd72f9a9b4adc71f04b3cddb58f8277352
mr. whalen <michael.whalen@sitechhs.com>
2020-10-21 08:09:34 -0400

    added async await notes

--- console.js
+++ console.js
@@ -1,5 +1,5 @@
 const pkmn = "alakazam";
-function getPkmn() {
+/* function getPkmn() {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`)
     .then((result) => {
       //console.log(result);
@@ -8,5 +8,13 @@ function getPkmn() {
     .then((data) => {
       console.log(data);
     });
+}
+getPkmn();
+ */
+async function getPkmn() {
+  const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`);
+  //console.log(result);
+  const data = await result.json();
+  console.log(data);
 }
 getPkmn();
