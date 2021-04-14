6df980d8ddea12b57e11317820c986430f1bddcc
mr. whalen <michael.whalen@sitechhs.com>
2021-04-14 13:21:53 -0400

    dghj

--- asyncDemo.js
+++ asyncDemo.js
@@ -1,6 +1,7 @@
 async function getData() {
   try {
     const result = await fetch(`https://pokeapi.co/api/v2/pokemon/pikkachu`);
+    //console.log(result);
     const data = await result.json();
     console.log(data);
   } catch (error) {
